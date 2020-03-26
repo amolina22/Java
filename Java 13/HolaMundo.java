a7b90e3d1b927835961b205d6780a1e6f7bd1536
Abel Molina S <abel.molweb@gmail.com>
2020-03-26 02:03:10 -0600

    Hola mundo java 13

---
+++ Java 13/HolaMundo.java
@@ -0,0 +1,18 @@
+/*
+ * Este es un comentario de varias líneas
+ */
+package com.msa.java13;
+
+/**
+ *
+ * @author Abel Molina S
+ */
+public class HolaMundo {
+
+    public static void main(String args[]) {
+
+        System.out.println("Hola Mundo Java 13");
+
+    }
+
+}
